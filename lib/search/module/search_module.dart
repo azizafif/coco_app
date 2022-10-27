763a6bdc7531ee3f78dfdf0e9688672a1010390e
Aziz <azizafif911@gmail.com>
2022-10-27 22:46:51 +0300

    last commit

--- lib/search/module/search_module.dart
+++ lib/search/module/search_module.dart
@@ -1,6 +1,6 @@
 import 'package:coco_app/api/modules/inheritance_provider.dart';
 import 'package:coco_app/search/controllers/search_controller.dart';
-import 'package:coco_app/search_view.dart';
+import 'package:coco_app/search/search_view.dart';
 import 'package:get/get.dart';
 
 final searchModule = GetPage(
